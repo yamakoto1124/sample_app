a463e95d971e6f944c798cf17e69961137165d9d
yamakoto1124 <293471220+yamakoto1124@>
2026-06-28 09:28:25 +0000

    [finish]3-6,7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,8 +9,8 @@ class ListsController < ApplicationController
    list = List.new(list_params)
      # 3. データをデータベースに保存するためのsaveメソッド実行
    list.save
-     # 4. トップ画面へリダイレクト
-   redirect_to '/top'
+     
+   redirect_to list_path(list.id)
   end
 
   def index
@@ -19,14 +19,28 @@ class ListsController < ApplicationController
   end
 
   def show
+     @list = List.find(params[:id])  
   end
 
   def edit
+     @list = List.find(params[:id])
+  end
+
+   def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)  
+  end
+
+  def destroy
+     list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト 
   end
 
 private
     # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,10 @@
 Rails.application.routes.draw do
-  get "lists/new"
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get "lists/show"
-  get "lists/edit"
-  get '/top' => 'homes#top'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'top' => 'homes#top'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  resources :lists
+ 
+ 
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+ 
 end
