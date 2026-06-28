3daee6534fb4fe806e068ae4accb7b8cd44c194a
yamakoto1124 <293471220+yamakoto1124@>
2026-06-28 07:46:24 +0000

    [finish]3-5

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,5 @@ Rails.application.routes.draw do
   # .../lists/1 や .../lists/3 に該当する
   get 'lists/:id' => 'lists#show', as: 'list'
   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
 end
